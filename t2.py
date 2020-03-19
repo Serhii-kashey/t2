b077e30dfc865630d86b60fce9497c9c4ca2e71d
Serhii-kashey <59534161+Serhii-kashey@users.noreply.github.com>
2020-03-19 22:35:55 +0200

    t2
    python Core
    using lienary alhoritms

---
+++ t2.py
@@ -0,0 +1,27 @@
+gramm=input('Gr's:? ')
+print("Kg's:=", int(gramm)/1000)
+print("Tonn =" byte//1024//1024)
+
+#byte=int(input("Byte? "))
+#print("kilobyte", byte//1024)
+#print("megabyte", byte//1024//1024)
+
+#a=input("1-st : " )
+#b=input("2-on : ")
+#a,b=b,a
+#print(a,b)
+
+#h=int(input('height '))
+#w=int(input('weight '))
+#print("Plosha",h*w)
+#print("perymetr",2*(h+w))
+
+#import math
+#r=float(input('radius '))
+#print("Plosha =", math.pi*r**2)
+#print("dovzhyna", math.pi*r*2)
+from math import *
+kat1=int(input("1-st katet: " ))
+kat2=int(input("2-st katet: " ))
+print("gipotenuza: ", sqrt(sum([kat1**2,kat2**2])))
+
